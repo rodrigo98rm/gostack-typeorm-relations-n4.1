03c6429617b3a6c2e4cecb2228e826f60536cc9b
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2020-08-03 14:56:03 -0300

    Customers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    // Check if email is in use
+    const user = await this.customersRepository.findByEmail(email);
+    if (user) {
+      throw new AppError('Email already in use', 400);
+    }
+
+    const newUser = await this.customersRepository.create({ name, email });
+
+    return newUser;
   }
 }
 
